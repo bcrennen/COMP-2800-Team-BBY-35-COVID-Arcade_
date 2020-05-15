af82755827eeca730a51ac614a9bde903f7da94f
Brennen Chiu <bchiyeung1996@gmail.com>
2020-05-15 12:55:11 -0700

    this is new stuff for html

--- scripts/firebase_api_brennen.js
+++ scripts/firebase_api_brennen.js
@@ -2,33 +2,11 @@ var firebaseConfig = {
     apiKey: "AIzaSyCmuxXOQm5Bjl9THSj50_3TCHWLC3_w_Yk",
     authDomain: "comp-2800-bby-35.firebaseapp.com",
     databaseURL: "https://comp-2800-bby-35.firebaseio.com/",
-    // projectId: "comp-2800-bby-35",
     storageBucket: "comp-2800-bby-35.appspot.com",
-    // messagingSenderId: "695889480465",
-    // appId: "1:695889480465:web:411755939bf2c44bd6dab0",
-    // measurementId: "G-L0C2S6WY22"
-// var firebaseConfig = {
-//     apiKey: "AIzaSyDVin5xwW35Iba0upHqJLgzeU4fpFnpvi4",
-//     authDomain: "final-grade-calculator-7efd1.firebaseapp.com",
-//     databaseURL: "https://final-grade-calculator-7efd1.firebaseio.com",
-//     projectId: "final-grade-calculator-7efd1",
-//     storageBucket: "final-grade-calculator-7efd1.appspot.com",
-//     messagingSenderId: "578696389905",
-//     appId: "1:578696389905:web:3b9220083a670b473b58b0"
+
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 var database = firebase.database();
-
-//const db = firebase.firestore();
-
-// get elements
-// const getUID = document.getElementById('email_field');
-
-// // create references 
-// const dbRefUID = firebase.database(). ref().child('user');
-
-// // sync object changes
-// dbRefUID.on('value', snap => console.log(snap.val()));
 
 
