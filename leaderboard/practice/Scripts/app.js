79fd37a08492f06cc585f119b262b5fa0d3de999
Siwoon Lim <mongle0629@gmail.com>
2020-05-21 16:38:39 -0700

    Blitzmode leaderboard updated

--- leaderboard/practice/Scripts/app.js
+++
@@ -1,7 +0,0 @@
-var config = {
-  apiKey: "AIzaSyCmuxXOQm5Bjl9THSj50_3TCHWLC3_w_Yk",
-  authDomain: "comp-2800-bby-35.firebaseapp.com",
-  databaseURL: "https://comp-2800-bby-35.firebaseio.com/",
-  storageBucket: "comp-2800-bby-35.appspot.com",
-};
-firebase.initializeApp(config);
